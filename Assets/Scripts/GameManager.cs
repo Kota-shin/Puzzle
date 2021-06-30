3f5d6c6aa5654c27d2efaa7a49c23c156b1267fa
KotaGit <Kota7arai@gmail.com>
2021-06-30 23:43:44 +0900

    the ball
    パズル

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int StageNum;
+    public bool isBallMoving;
+    public GameObject ballPrefab;
+    public GameObject ball;
+    public GameObject goButton;
+    public GameObject retryButton;
+    public GameObject clearText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        retryButton.SetActive(false);
+        isBallMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PushGoButton()
+    {
+        Rigidbody2D rd = ball.GetComponent<Rigidbody2D>();
+        rd.isKinematic = false;
+
+        retryButton.SetActive(true);
+        goButton.SetActive(false);
+        isBallMoving = true;
+    }
+
+    public void PushRetryButton()
+    {
+        Destroy(ball);
+
+        ball = (GameObject)Instantiate(ballPrefab);
+        retryButton.SetActive(false);
+        goButton.SetActive(true);
+        isBallMoving = false;
+    }
+
+    public void StageClear()
+    {
+        clearText.SetActive(true);
+        retryButton.SetActive(false);
+    }
+
+}

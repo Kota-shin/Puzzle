2d6c4022d5affe0d127099bd5efcec26aaca1a9c
KotaGit <Kota7arai@gmail.com>
2021-07-05 22:24:54 +0900

    ステージ選択の実装
    ステージ選択の実装

---
+++ Assets/Scripts/StgeSelectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StgeSelectManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ステージ選択ボタンの処理
+    public void PushStageSelectButton(int stageNo)
+    {
+        SceneManager.LoadScene("PuzzleScene" + stageNo);    //ゲームシーンへ
+    }
+}

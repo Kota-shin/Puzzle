30ad10d77625b6fa198f54ebc4b2ad6cc8b6a108
KotaGit <Kota7arai@gmail.com>
2021-07-10 17:44:48 +0900

    ステージ４，５実装
    ステージ４，５実装

---
+++ Assets/Scripts/UnmovableBoxCtr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnmovableBoxCtr : MonoBehaviour
+{
+    float rotSpeed;             //障害物の回転速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotSpeed = -2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotSpeed);
+    }
+}
